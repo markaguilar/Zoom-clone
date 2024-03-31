7c8cbb1e6b720b0bd36c98a61d7dfca298f2f62c
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2024-03-31 20:07:25 +0800

    added clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+import MobileNav from "@/components/MobileNav";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        {/*    clerk user management*/}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/meeting(.*)",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)",
+  ],
+};
